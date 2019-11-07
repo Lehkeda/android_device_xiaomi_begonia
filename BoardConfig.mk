4e756aa2a765f60b4d7d78fed794929e82861324
Hesham El-sewify (LehKeda) <computer.boy71@yahoo.com>
2019-11-07 08:52:43 +0200

    fix build error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ DEVICE_PATH := device/xiaomi/begonia
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := cortex-a75
 
 TARGET_2ND_ARCH := arm64
